942f037690a2ad41a37e5beef12b2efea3c7431c
emanuelvalente <emanuelvalente@gmail.com>
2015-02-11 22:44:31 -0700

    fix memmory allocs

--- hw1/sortSeq.c
+++ hw1/sortSeq.c
@@ -59,7 +59,7 @@ int createArrayfromFile(char *filename, char ***array_)
   array = (char**)malloc(sizeof(char*) * spaces);
   
   do {
-    array[count] = (char*)malloc(sizeof(char) * strlen(ret));
+    array[count] = (char*)malloc(sizeof(char) * (strlen(ret) + 1));
     strcpy(array[count++], ret);
     //printf("%s\n", ret);
    
@@ -77,15 +77,63 @@ int createArrayfromFile(char *filename, char ***array_)
   
 }
 
+void swap(char **array, int i, int j) {
+  char *tmp;
+  int len_i = strlen(array[i]);
+  int len_j = strlen(array[j]);
+  
+  tmp = (char*)malloc(sizeof(char)* (len_i + 1));
+  strcpy(tmp, array[i]);
+  
+  if(len_i < len_j) 
+    array[i] = (char*)realloc(array[i], sizeof(char) * (len_j +1));
+  
+  else if(len_i > len_j) 
+    array[j] = (char*)realloc(array[j], sizeof(char) * (len_i +1));
+  
+  strcpy(array[i], array[j]);
+  strcpy(array[j], tmp); 
+  
+  free(tmp);
+  
+  
+}
+
+int partition(char **array, int left, int right) {
+  int i, j;
+  
+  i = left;
+  for(j = left + 1; j <= right; ++j) {
+    if(strcmp(array[j], array[left]) < 0) {
+      ++i;
+      swap(array, i, j);
+    }
+  }
+  
+  swap(array, left, i);
+  
+  return i;
+  
+}
+
+void quick_sort(char **array, int left, int right) {
+  int r;
+  
+  if(right > left) {
+    r = partition(array, left, right);
+    
+    quick_sort(array, left, r-1);
+    quick_sort(array, r+1, right);    
+  }
+  
+  
+}
   
 
 int main(int argc, char **argv) {
   
   char *filename;
   char **array;
-  char c;
-  FILE *myfile;
-  int length, count;
   int array_length;
   
   if(argc < 2) {
@@ -93,10 +141,11 @@ int main(int argc, char **argv) {
     exit(1);
   }
   
-  filename = argv[1];
-  
+  filename = argv[1]; 
   //creates the array
   array_length = createArrayfromFile(filename, &array);
+  
+  quick_sort(array, 0, array_length -1);
   
   
   //print the content case debug is set
